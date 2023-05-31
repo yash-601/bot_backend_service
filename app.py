83c7599f1528cae850030faec506783ba9a2a737
yash-601 <68612868+yash-601@users.noreply.github.com>
2023-05-31 18:00:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 def hello_world():
 	d, links = {}, {}
 	link = 1
-	d['Query'] = str(request.args['Query'])
-	for j in search(d['Query'], tld="co.in", num=3, stop=3, pause=2):
+	query = request.args['Query'].split(",")
+	d['Query'] = query[0] + query[1]
+	for j in search(d['Query'], tld="co.in", num=2, stop=2, pause=2):
+		links[f'{link}'] = j
+		link += 1 
+	d['Query'] = query[0] + query[2]
+	for j in search(d['Query'], tld="co.in", num=2, stop=2, pause=2):
+		links[f'{link}'] = j
+		link += 1 
+	d['Query'] = query[0] + query[3]
+	for j in search(d['Query'], tld="co.in", num=1, stop=1, pause=2):
 		links[f'{link}'] = j
 		link += 1 
 	return jsonify(links)
